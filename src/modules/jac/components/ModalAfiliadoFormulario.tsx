533f1ecebf17824e7208c9e546dc8377f79a6ac9
everson <eversonrestrepo@unicauca.edu.co>
2026-06-06 21:03:23 -0500

    Arreglo bug que agregar o actualizador afiliado a indigena y boton de importar excel que no debe aparecer a un operador

--- src/modules/jac/components/ModalAfiliadoFormulario.tsx
+++ src/modules/jac/components/ModalAfiliadoFormulario.tsx
@@ -293,7 +293,7 @@ export function ModalAfiliadoFormulario({
               <select name="grupoEtnico" value={formData.grupoEtnico} onChange={handleChange} className="w-full px-3 py-2 border border-gray-200 dark:border-gray-600 rounded text-sm bg-white dark:bg-gray-700 text-gray-800 dark:text-gray-100">
                 <option value="">Seleccionar...</option>
                 <option value="Afro">Afro</option>
-                <option value="Indígena">Indígena</option>
+                <option value="Indigena">Indígena</option>
                 <option value="Mestizo">Mestizo</option>
                 <option value="Campesino">Campesino</option>
               </select>
